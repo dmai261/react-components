2aad798fbddc4d3e3e140453c8ff7fdb8e723838
David <david@Davids-MacBook-Air.local>
2018-08-13 11:41:11 -0700

    Wrote a GroceryList that takes in two componenets

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+var App = () => (
+  <div>Some cliche salutation
+    <h2>To Do List </h2>
+    <toDoList />
+  </div>
+);
+var GroceryList = () => (
+  <div>
+    <Ham />
+    <Egg /> 
+  </div>
+);
+
+var Ham = () => (
+  <div>Ham</div>
+)
+
+var Egg = () => (
+  <div>Egg</div>
+)
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
